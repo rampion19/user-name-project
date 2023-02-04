5f3b5ac6a7c5466a199bff8310894d768e7be68f
rampion19 <princegupta.pg19@gmail.com>
2023-02-04 16:54:43 +0530

    created wrapper component and use react portal

--- src/components/UI/ErrorModel.js
+++ src/components/UI/ErrorModel.js
@@ -1,25 +1,46 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import './ErrorModel.css'
+
+const BackDrop = props => {
+    return <div className="backdrop" onClick={props.onConfirm} />
+}
+
+const ModalOverlay = props => {
+    return (
+        <div>
+            <div className="modal">
+                <Card>
+                    <header className="header">
+                        <h2>{props.title}</h2>
+                    </header>
+                    <div className="content">
+                        <p>{props.message}</p>
+                    </div>
+                    <footer className="action">
+                        <Button onClick={props.onConfirm}>okay</Button>
+                    </footer>
+                </Card>
+            </div>
+        </div>
+    )
+}
 const ErrorModel = props => {
     return (
-    <div>
-        <div className="backdrop" onClick = {props.onConfirm} />
-    <div className="modal">
-        <Card>
-        <header className="header">
-            <h2>{props.title}</h2>
-        </header>
-        <div className="content">
-            <p>{props.message}</p>
-        </div>
-        <footer className="action">
-            <Button onClick = {props.onConfirm}>okay</Button>
-        </footer>
-        </Card>
-    </div>
-    </div>
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <BackDrop onConfirm={props.onConfirm} />,
+                document.getElementById("backdrop-root"))}
+
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onConfirm} />,
+                document.getElementById("overlay-root"))}
+        </React.Fragment>
 
     )
 }
